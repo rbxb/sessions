a41e4598a59bef92087869707845e2332fef8702
rbxb <26235793+rbxb@users.noreply.github.com>
2019-03-14 12:29:48 -0700

    Added README

--- sessions.go
+++ sessions.go
@@ -3,32 +3,26 @@ package sessions
 import (
 	"time"
 	"net/http"
-	"strings"
-	"strconv"
 	"github.com/google/uuid"
 )
 
 type session struct {
-	id int
 	uuid string
 	expires time.Time
 	v interface{}
-	next * session
 }
 
 type Store struct {
-	* session
-	id int
 	lock chan byte
 	lifetime time.Duration
+	sessions [] * session
 }
 
 func NewStore(lifetime time.Duration) * Store {
 	return &Store{
-		session: 	nil,
-		id: 		0,
 		lock: 		make(chan byte, 1),
 		lifetime: 	lifetime,
+		sessions: 	make([] * session, 0),
 	}
 }
 
@@ -39,24 +33,13 @@ func(store * Store) NewSession(w http.ResponseWriter, v interface{}) {
 		v: 			v,
 	}
 
-	<- store.lock
-	s.id = store.id
-	store.id++
-	cur := store.session
-	if cur == nil {
-		store.session = s
-	} else {
-		for cur.next != nil {
-			cur = cur.next
-		}
-		cur.next = s
-	}
 	store.lock <- 0
+	store.sessions = append(store.sessions, s)
+	<- store.lock
 
-	cookieValue := strconv.Itoa(s.id) + "," + s.uuid
 	cookie := http.Cookie{
 		Name: 		"session",
-		Value: 		cookieValue,
+		Value: 		s.uuid,
 		Expires: 	s.expires,
 	}
 	http.SetCookie(w, &cookie)
@@ -68,53 +51,31 @@ func(store * Store) Get(w http.ResponseWriter, req * http.Request) interface{} {
 		return nil
 	}
 
-	split := strings.Split(cookie.Value, ",")
-	id, err := strconv.Atoi(split[0])
-	if err != nil {
-		return nil
-	}
-	uuid := split[1]
-
-	<- store.lock
 	var v interface{} = nil
-	for s := store.session; s != nil; s = s.next {
-		if s.id == id {
-			if s.uuid == uuid {
-				if s.expires.Unix() > time.Now().Unix() {
-					s.expires.Add(10 * time.Minute)
-					cookie.Expires = s.expires
-					http.SetCookie(w, cookie)
-					v = s.v
-				} else {
-					store.del(s)
-				}
+	store.lock <- 0
+	for i, s := range store.sessions {
+		if s.uuid == cookie.Value {
+			if s.expires.Unix() > time.Now().Unix() {
+				s.expires = time.Now().Add(store.lifetime)
+				cookie.Expires = s.expires
+				http.SetCookie(w, cookie)
+				v = s.v
+				break
+			} else {
+				store.sessions = append(store.sessions[:i], store.sessions[i+1:]...)
 			}
 		}
 	}
-	store.lock <- 0
+	<- store.lock
 	return v
 }
 
-func(store * Store) del(s * session) {
-	if s != nil {
-		<- store.lock
-		c := store.session
-		for c != nil && c.next != s {
-			c = c.next
-		}
-		if c != nil {
-			c.next = c.next.next
-		}
-		store.lock <- 0
-	}
-}
-
 func(store * Store) Clean() {
-	<- store.lock
-	for s := store.session; s != nil; s = s.next {
+	store.lock <- 0
+	for i, s := range store.sessions {
 		if s.expires.Unix() < time.Now().Unix() {
-			store.del(s)
+			store.sessions = append(store.sessions[:i], store.sessions[i+1:]...)
 		}
 	}
-	store.lock <- 0
+	<- store.lock
 }
